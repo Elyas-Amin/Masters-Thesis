8188c0a05e443046ca3bdea30bd7ee7a99f733aa
Elyas Amin <elyasamin26@gmail.com>
2024-08-25 13:20:44 -0400

    entity grid code start

---
+++ CoherenceModels/EntityGrid.java
@@ -0,0 +1,21 @@
+package EntityGrid;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.ling.CoreAnnotations.PartOfSpeechAnnotations;
+
+public class EntityGrid {
+    protected static final char EMPTY = '-';
+    protected static final char O = 'O';
+    protected static final char S = 'S';
+    protected static final char X = 'X';
+
+    
+
+}
